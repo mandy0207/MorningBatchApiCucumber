ba5e7097021e8a9719e9a2aa873a8537d3b2f0ba
unknown <mandyatwork24@gmail.com>
2025-05-14 08:01:56 +0530

    reports

--- src/test/java/com/library/cucumberOptions/TestRunner.java
+++ src/test/java/com/library/cucumberOptions/TestRunner.java
@@ -4,7 +4,10 @@ import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 
 @CucumberOptions(features="src/main/java/featurefiles",
-glue="stepDefinitions", tags="@Sanity")
+glue="stepDefinitions", tags="@Smoke",
+plugin={"com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter: ",
+		"html:target/HTMLReports/cucumber.html",
+		"json:target/jsonReports/cucumber.json"})
 public class TestRunner extends AbstractTestNGCucumberTests {
 
 	
---
+++ src/test/java/stepDefinitions/DeleteBookStepDefintion.java
@@ -0,0 +1,14 @@
+package stepDefinitions;
+
+import utils.ScenarioContext;
+
+public class DeleteBookStepDefintion {
+
+	private ScenarioContext  scenarioContext;
+	public DeleteBookStepDefintion (ScenarioContext	scenarioContext) {
+		this. scenarioContext= scenarioContext;
+	}
+	
+	
+	 
+}
